5f259e696be6dbca8407bba687b6c068cb5ce0eb
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-11-06 03:41:47 -0800

    5. Can you C me now?

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+
+    newString = ""
+
+    for j in my_string:
+        if j == 'c' or j == "C":
+            pass
+        else:
+            newString = newString + j
+    return new_string
