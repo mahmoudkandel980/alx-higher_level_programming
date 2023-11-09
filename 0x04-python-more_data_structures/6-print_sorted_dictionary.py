69500567369838282c234958b740c708d45f0c3b
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-11-08 18:23:09 -0800

    udpate number_keys

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    li = a_dictionary.keys()
-    for el in sorted(sortList):
-        print("{:s}: {}".format(el, a_dictionary[el]))
+    for element in sorted(a_dictionary):
+        print('{}: {}'.format(element, a_dictionary[element]))
