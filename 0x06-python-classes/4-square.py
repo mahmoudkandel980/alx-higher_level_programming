227d315dfde94711ed7f677787b30096ef6a76c7
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-11-21 14:54:10 -0800

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Create a Class Square with size, area, getters and setters"""
+
+
+class Square:
+    """Square Class"""
+
+    def __init__(self, size=0):
+        """Constructor of a size Square"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Method to get the Square area"""
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """Getter of the private size attribute"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for the private size attribute"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
