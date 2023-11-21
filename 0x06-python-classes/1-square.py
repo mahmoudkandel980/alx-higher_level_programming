e084096598e04e01e71fd916ec66eda07a3c6ebc
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-11-21 14:39:14 -0800

    1. Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""Class Square"""
+
 
 class Square:
+    """Class to create a square size"""
 
     def __init__(self, squareSize):
+        """Constructor"""
         self.__size = squareSize
