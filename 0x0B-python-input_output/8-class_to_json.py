d7a298f2efd102f9ee631e07d33401f8d1928967
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-12-05 12:42:59 -0800

    8. Class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+This is a boilerplate pipeline 'file'
+generated using Kedro 0.18.4
+"""
+
+
+def class_to_json(obj):
+    """
+    Convert object to JSON
+    Args:
+        obj: object
+    """
+    return obj.__dict__
