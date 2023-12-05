2ac615b52bd9428a0d11effc96c81cdd8e74e3f0
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-12-05 12:28:41 -0800

    1. Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""File reading function"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8) and returns the number of
+    characters written
+
+    Args:
+        filename (str): The name of the file
+        text (str): The text to write
+    Returns:
+        The number of characters written
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
